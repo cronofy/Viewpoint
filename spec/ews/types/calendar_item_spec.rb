72a784f484013ffeb04adf1e78128343f10102a3
Stephen Binns <stephen.binns@gmail.com>
2017-05-25 10:55:02 +0100

    Added spec for organizer parsing

--- spec/ews/types/calendar_item_spec.rb
+++ spec/ews/types/calendar_item_spec.rb
@@ -11,4 +11,22 @@ describe Viewpoint::EWS::Types::CalendarItem do
     end
   end
 
+  describe "#organizer" do
+    it "maps the organizer type passed" do
+      calitem = described_class.new(nil, {
+        :organizer=>{:elems=>[{:mailbox=>{:elems=>[{:name=>{:text=>"Organizer Name"}}, {:email_address=>{:text=>"organizer@example.com"}}, {:routing_type=>{:text=>"EX"}}]}}]}
+      })
+      expect(calitem.organizer.name).to eql("Organizer Name")
+      expect(calitem.organizer.email).to eql("organizer@example.com")
+      expect(calitem.organizer.email_address).to eql("organizer@example.com")
+    end
+  end
+
+  describe "#my_response_type" do
+    it "maps the response type passed" do
+      calitem = described_class.new(nil, { my_response_type: { text: "Organizer" }})
+      expect(calitem.my_response_type).to eql("Organizer")
+    end
+  end
+
 end
