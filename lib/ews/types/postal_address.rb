6b89d87d6ac32f919e35112be0921bde026e8406
Stephen Binns <stephen.binns@gmail.com>
2017-10-10 14:02:12 +0100

    Added support for reading extended location

---
+++ lib/ews/types/postal_address.rb
@@ -0,0 +1,52 @@
+module Viewpoint::EWS::Types
+  class PostalAddress
+    include Viewpoint::EWS
+    include Viewpoint::EWS::Types
+
+    POSTAL_ADDRESS_KEY_PATHS = {
+      street:            [:street],
+      city:              [:city],
+      state:             [:state],
+      country:           [:country],
+      postal_code:       [:postal_code],
+      type:              [:type],
+      latitude:          [:latitude],
+      longitude:         [:longitude],
+      accuracy:          [:accuracy],
+      altitude:          [:altitude],
+      altitude_accuracy: [:altitude_accuracy],
+      location_uri:      [:location_uri],
+      location_source:   [:location_source],
+    }
+    POSTAL_ADDRESS_KEY_TYPES = { }
+    POSTAL_ADDRESS_KEY_ALIAS = { }
+
+    def initialize(ews, postal_address)
+      @ews = ews
+      @ews_item = postal_address
+      simplify!
+    end
+
+    private
+
+    def simplify!
+      @ews_item = @ews_item.inject({}){|m,o|
+        m[o.keys.first] = o.values.first[:text] || o.values.first[:elems];
+        m
+      }
+    end
+
+    def key_paths
+      @key_paths ||= super.merge(POSTAL_ADDRESS_KEY_PATHS)
+    end
+
+    def key_types
+      @key_types ||= super.merge(POSTAL_ADDRESS_KEY_TYPES)
+    end
+
+    def key_alias
+      @key_alias ||= super.merge(POSTAL_ADDRESS_KEY_ALIAS)
+    end
+
+  end # PostalAddress
+end # Viewpoint::EWS::Types
